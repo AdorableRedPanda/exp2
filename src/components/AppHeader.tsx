681c4b17ce0b6e473168d00a6cf84047d324aafb
konstantin <gktin@yandex.ru>
2025-09-24 01:20:29 +0400

    change name

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/shadcn/components';
 
 export const AppHeader: React.FC = () => (
 	<header className="sticky bg-primary text-background px-2 py-2 row-start-1 flex gap-1 flex-wrap items-center justify-between">
-		<h1 className="text-2xl font-bold flex-1">Transactions</h1>
+		<h1 className="text-2xl font-bold flex-1">Transactions Profile</h1>
 		<Link download href="/api/download/">
 			<Button
 				className="text-md w-full flex items-center gap-1"
